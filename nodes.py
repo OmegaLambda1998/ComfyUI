c9066cfb890caa27df74ebd1a98d2350ad824112
Patrick Armstrong <patrick.armstrong@anu.edu.au>
2024-07-23 11:44:16 +1000

    Stop comfy_audio extra node from loading

--- nodes.py
+++ nodes.py
@@ -2033,7 +2033,7 @@ def init_builtin_extra_nodes():
         "nodes_attention_multiply.py",
         "nodes_advanced_samplers.py",
         "nodes_webcam.py",
-        "nodes_audio.py",
+        #"nodes_audio.py",
         "nodes_sd3.py",
         "nodes_gits.py",
         "nodes_controlnet.py",
